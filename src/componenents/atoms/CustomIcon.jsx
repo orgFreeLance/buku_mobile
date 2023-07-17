e69ee259a7168994ed3c878a9817b4ce75b3d066
Tekango <sacre.tekango@gmail.com>
2023-07-17 14:43:57 +0100

    feat: create main header component

--- src/componenents/atoms/CustomIcon.jsx
+++ src/componenents/atoms/CustomIcon.jsx
@@ -2,7 +2,7 @@ import { Image } from "expo-image";
 import { Flex } from "native-base";
 
 const CustomIcon = ({iconModule, width, height}) => (
-    <Flex>
+    <Flex style={{marginLeft: 5}}>
         <Image style={{ width, height }} source={iconModule} contentFit="contain"
             transition={1000} />
     </Flex>
--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -2,24 +2,16 @@ import { Flex, ScrollView, StatusBar, Text, View } from 'native-base';
 import * as React from 'react';
 import { StyleSheet, } from 'react-native';
 import CTAContainer from '../../componenents/organisms/CTAContainer';
-import { Image } from 'expo-image';
 import { height, width } from '../../constants/nativeSizes';
-import BillsStatus from '../../componenents/molecules/BillsStatus';
+import Header1 from '../../componenents/organisms/Header1';
 
-const logo = require("../../../assets/Logo.png");
 
 const Login = () => {
     return <View style={styles.container}>
         <StatusBar barsTyle={"light-content"} />
         <ScrollView style={{ paddingHorizontal: width(5), paddingVertical: height(1) }}>
             <Flex>
-                <Flex direction='row' justifyContent={"space-between"}>
-                    <View style={{ width: 120, height: 32 }}>
-                        <Image style={{ width: "100%", height: 32 }} source={logo} contentFit="contain"
-                            transition={1000} />
-                    </View>
-                    <BillsStatus />
-                </Flex>
+                <Header1 />
                 <Flex>
                     <Text>Title</Text>
                 </Flex>
