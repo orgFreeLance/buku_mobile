5c2286238f7b78b7152fe60e1ceba538161ed29e
jeereq <mingandajeereq@gmail.com>
2023-11-03 00:25:33 +0100

    accept validations from merge

--- src/navigations/Main.jsx
+++ src/navigations/Main.jsx
@@ -45,7 +45,6 @@ const forFade = ({ current }) => ({
 });
 const Stack = createStackNavigator();
 
-<<<<<<< HEAD
 const MainNavigation = () => {
   const userIsAuth = userStore((state) => state.isAuth);
   return (
@@ -110,59 +109,4 @@ const MainNavigation = () => {
   );
 };
 
-=======
-const MainNavigation = observer(() => {
-  const [userAuth, setIsAuth] = useState(false);
-  const [userConfirm, setConfirmed] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [confirmed, isAuth] = userStore(
-    (state) => [state.confirmed, state.isAuth],
-    shallow
-  );
-
-  const getAuthKeys = async () => {
-    setLoading(true);
-    const isAuth =
-      parseInt(await SecureStore.getItemAsync("isAuth"), 10) === 1 || isAuth;
-    const confirmed =
-      parseInt(await SecureStore.getItemAsync("confirmed"), 10) === 1 ||
-      confirmed;
-    setIsAuth(isAuth);
-    setConfirmed(confirmed);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getAuthKeys();
-  }, [confirmed]);
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      {!loading ? (
-        <>
-
-          {confirmed ? (
-            <Stack.Group>
-              <Stack.Screen name='Bottom' component={Bottom} />
-            </Stack.Group>
-          ) : (
-            <Stack.Group>
-              <Stack.Screen name='Start' component={Start} />
-              <Stack.Screen name='Login' component={Login} />
-              <Stack.Screen name='Signup' component={Signup} />
-              <Stack.Screen name='VerifyCode' component={VerifyCode} />
-            </Stack.Group>
-          )}
-        </>
-      ) : (
-        <Stack.Screen name='Splash' component={Splash} />
-      )}
-    </Stack.Navigator>
-  );
-});
-
->>>>>>> fbf37147b99988978f7f318dfed95a706aa0183b
 export default MainNavigation;
