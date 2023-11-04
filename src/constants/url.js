c498080c7446116b2d08c7c0bc4a9ec8998836be
jeereq <mingandajeereq@gmail.com>
2023-11-04 16:19:18 +0100

    feat: create new block for try catch

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,4 +1,5 @@
 export const tomeURl = (id) => `/tome-infos/tome-increment/${id}?populate=creator,book`
 export const tomesURl = "/tomes?fields[0]=picture&fields[1]=name&fields[2]=id&fields[3]=userViews&fields[4]=likesNumber&fields[5]=coinsPrice"
 export const categoriesURl = "/categories?fields[0]=picture&fields[1]=name&fields[2]=id"
-export const categoryOfTomeURl = (id)=>`/tome-infos/categories/${id}`
+export const categoryOfTomeURl = (id) => `/tome-infos/categories/${id}`
+export const bookByGenreURL = (id) => `/tome-infos/tomes-by-category/${id}`
