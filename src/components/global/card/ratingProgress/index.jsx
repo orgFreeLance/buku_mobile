cef8f06154432da1fc67c876ad7a0e076e5c373e
jeereq <mingandajeereq@gmail.com>
2024-03-09 20:12:12 +0100

    refactor: design of card tratingprogress

--- src/components/global/card/ratingProgress/index.jsx
+++ src/components/global/card/ratingProgress/index.jsx
@@ -2,20 +2,46 @@ import { Text, View } from "react-native";
 import theme from "../../../../constants/theme";
 
 export default function CardRatingProgress({ pourcentage = 80, number = 5 }) {
-    return <>
-        <View style={{ flexDirection: "row", alignItems: "center", alignContent: "center", marginBottom: 5 }}>
-            <Text style={{ textAlignVertical: "center", marginRight: 3 }}>{number}</Text>
-            <View style={{
-                position: "relative",
-                borderRadius: 5,
-                height: 7,
-                width: `${pourcentage * 2}%`,
-                backgroundColor: theme.colors.brand.secondary
-            }}>
-                <View style={{ position: "absolute", borderRadius: 10, right: 0, transform: [{ translateX: 25 }, { translateY: -4 }] }}>
-                    <Text >{pourcentage}%</Text>
-                </View>
-            </View>
+  return (
+    <>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          alignContent: "center",
+          marginBottom: 5,
+        }}
+      >
+        <Text
+          style={{
+            textAlignVertical: "center",
+            fontWeight: "500",
+            marginRight: 3,
+          }}
+        >
+          {number}
+        </Text>
+        <View
+          style={{
+            position: "relative",
+            borderRadius: 5,
+            height: 7,
+            width: `${pourcentage * 2}%`,
+            backgroundColor: theme.colors.brand.secondary,
+          }}
+        >
+          <View
+            style={{
+              position: "absolute",
+              borderRadius: 10,
+              right: 0,
+              transform: [{ translateX: 20 }, { translateY: -3 }],
+            }}
+          >
+            <Text style={{ fontSize: 10 }}>{pourcentage}%</Text>
+          </View>
         </View>
+      </View>
     </>
+  );
 }
